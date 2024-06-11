7dd16b49eb86d9b63566a565e8a591c20c08da5f
Daniel Fernandes <danieldeqfernandes@gmail.com>
2024-06-11 19:15:47 -0300

    Seção de notícias iniciada

--- codigo/app.js
+++ codigo/app.js
@@ -134,117 +134,117 @@ var ultimos_jogos = [
     },
 ]
 
-function atualizar(){
+function atualizar() {
     // card 1 - proximos jogos
-        // nome do game
-        document.querySelector("#nome_jogo1").innerHTML = proximos_jogos[0].jogo
-        // campeonato
-        document.querySelector("#campeonato_1").innerHTML = proximos_jogos[0].campeonato
-        // data
-        document.querySelector("#data_1").innerHTML = proximos_jogos[0].data
-        // time 1
-        document.querySelector("#imagem_time_1").src = proximos_jogos[0].times.time_a.img
-        // time 2
-        document.querySelector("#imagem_time_2").src = proximos_jogos[0].times.time_b.img
+    // nome do game
+    document.querySelector("#nome_jogo1").innerHTML = proximos_jogos[0].jogo
+    // campeonato
+    document.querySelector("#campeonato_1").innerHTML = proximos_jogos[0].campeonato
+    // data
+    document.querySelector("#data_1").innerHTML = proximos_jogos[0].data
+    // time 1
+    document.querySelector("#imagem_time_1").src = proximos_jogos[0].times.time_a.img
+    // time 2
+    document.querySelector("#imagem_time_2").src = proximos_jogos[0].times.time_b.img
 
     // card 2
-        // nome do game
-        document.querySelector("#nome_jogo2").innerHTML = proximos_jogos[1].jogo
-        // campeonato
-        document.querySelector("#campeonato_2").innerHTML = proximos_jogos[1].campeonato
-        // data
-        document.querySelector("#data_2").innerHTML = proximos_jogos[1].data
-        // time 1
-        document.querySelector("#imagem_time_3").src = proximos_jogos[1].times.time_a.img
-        // time 2
-        document.querySelector("#imagem_time_4").src = proximos_jogos[1].times.time_b.img
+    // nome do game
+    document.querySelector("#nome_jogo2").innerHTML = proximos_jogos[1].jogo
+    // campeonato
+    document.querySelector("#campeonato_2").innerHTML = proximos_jogos[1].campeonato
+    // data
+    document.querySelector("#data_2").innerHTML = proximos_jogos[1].data
+    // time 1
+    document.querySelector("#imagem_time_3").src = proximos_jogos[1].times.time_a.img
+    // time 2
+    document.querySelector("#imagem_time_4").src = proximos_jogos[1].times.time_b.img
 
     // card 3 - ultimos jogos
-        // nome do game
-        document.querySelector("#nome_jogo3").innerHTML = ultimos_jogos[0].jogo
-        // campeonato
-        document.querySelector("#campeonato_3").innerHTML = ultimos_jogos[0].campeonato
-        // data
-        document.querySelector("#data_3").innerHTML = ultimos_jogos[0].data
-        // time 1
-        document.querySelector("#imagem_time_5").src = ultimos_jogos[0].times.time_a.img
-        // time 2
-        document.querySelector("#imagem_time_6").src = ultimos_jogos[0].times.time_b.img
-        // placar 1
-        document.querySelector("#placar_1").innerHTML = ultimos_jogos[0].times.time_a.placar
-        // placar 2
-        document.querySelector("#placar_2").innerHTML = ultimos_jogos[0].times.time_b.placar
+    // nome do game
+    document.querySelector("#nome_jogo3").innerHTML = ultimos_jogos[0].jogo
+    // campeonato
+    document.querySelector("#campeonato_3").innerHTML = ultimos_jogos[0].campeonato
+    // data
+    document.querySelector("#data_3").innerHTML = ultimos_jogos[0].data
+    // time 1
+    document.querySelector("#imagem_time_5").src = ultimos_jogos[0].times.time_a.img
+    // time 2
+    document.querySelector("#imagem_time_6").src = ultimos_jogos[0].times.time_b.img
+    // placar 1
+    document.querySelector("#placar_1").innerHTML = ultimos_jogos[0].times.time_a.placar
+    // placar 2
+    document.querySelector("#placar_2").innerHTML = ultimos_jogos[0].times.time_b.placar
 
     // card 4
-        // nome do game
-        document.querySelector("#nome_jogo4").innerHTML = ultimos_jogos[1].jogo
-        // campeonato
-        document.querySelector("#campeonato_4").innerHTML = ultimos_jogos[1].campeonato
-        // data
-        document.querySelector("#data_4").innerHTML = ultimos_jogos[1].data
-        // time 1
-        document.querySelector("#imagem_time_7").src = ultimos_jogos[1].times.time_a.img
-        // time 2
-        document.querySelector("#imagem_time_8").src = ultimos_jogos[1].times.time_b.img
-        // placar 1
-        document.querySelector("#placar_3").innerHTML = ultimos_jogos[1].times.time_a.placar
-        // placar 2
-        document.querySelector("#placar_4").innerHTML = ultimos_jogos[1].times.time_b.placar
+    // nome do game
+    document.querySelector("#nome_jogo4").innerHTML = ultimos_jogos[1].jogo
+    // campeonato
+    document.querySelector("#campeonato_4").innerHTML = ultimos_jogos[1].campeonato
+    // data
+    document.querySelector("#data_4").innerHTML = ultimos_jogos[1].data
+    // time 1
+    document.querySelector("#imagem_time_7").src = ultimos_jogos[1].times.time_a.img
+    // time 2
+    document.querySelector("#imagem_time_8").src = ultimos_jogos[1].times.time_b.img
+    // placar 1
+    document.querySelector("#placar_3").innerHTML = ultimos_jogos[1].times.time_a.placar
+    // placar 2
+    document.querySelector("#placar_4").innerHTML = ultimos_jogos[1].times.time_b.placar
 
-     
+
 }
 let i = 0
-function carrosel_direita(){
-     // nome do game
-     document.querySelector("#nome_jogo1").innerHTML = proximos_jogos[i + 2].jogo
-     // campeonato
-     document.querySelector("#campeonato_1").innerHTML = proximos_jogos[i + 2].campeonato
-     // data
-     document.querySelector("#data_1").innerHTML = proximos_jogos[i + 2].data
-     // time 2
-     document.querySelector("#imagem_time_1").src = proximos_jogos[i + 2].times.time_a.img
-     // time 2
-     document.querySelector("#imagem_time_2").src = proximos_jogos[i + 2].times.time_b.img
-    
- // card 2
-     // nome do game
-     document.querySelector("#nome_jogo2").innerHTML = proximos_jogos[i + 3].jogo
-     // campeonato
-     document.querySelector("#campeonato_2").innerHTML = proximos_jogos[i + 3].campeonato
-     // data
-     document.querySelector("#data_2").innerHTML = proximos_jogos[i + 3].data
-     // time 3
-     document.querySelector("#imagem_time_3").src = proximos_jogos[i + 3].times.time_a.img
-     // time 2
-     document.querySelector("#imagem_time_4").src = proximos_jogos[i + 3].times.time_b.img
+function carrosel_direita() {
+    // nome do game
+    document.querySelector("#nome_jogo1").innerHTML = proximos_jogos[i + 2].jogo
+    // campeonato
+    document.querySelector("#campeonato_1").innerHTML = proximos_jogos[i + 2].campeonato
+    // data
+    document.querySelector("#data_1").innerHTML = proximos_jogos[i + 2].data
+    // time 2
+    document.querySelector("#imagem_time_1").src = proximos_jogos[i + 2].times.time_a.img
+    // time 2
+    document.querySelector("#imagem_time_2").src = proximos_jogos[i + 2].times.time_b.img
+
+    // card 2
+    // nome do game
+    document.querySelector("#nome_jogo2").innerHTML = proximos_jogos[i + 3].jogo
+    // campeonato
+    document.querySelector("#campeonato_2").innerHTML = proximos_jogos[i + 3].campeonato
+    // data
+    document.querySelector("#data_2").innerHTML = proximos_jogos[i + 3].data
+    // time 3
+    document.querySelector("#imagem_time_3").src = proximos_jogos[i + 3].times.time_a.img
+    // time 2
+    document.querySelector("#imagem_time_4").src = proximos_jogos[i + 3].times.time_b.img
 }
 
-function carrosel_esquerda(){
-     // nome do game
-     document.querySelector("#nome_jogo1").innerHTML = proximos_jogos[i].jogo
-     // campeonato
-     document.querySelector("#campeonato_1").innerHTML = proximos_jogos[i].campeonato
-     // data
-     document.querySelector("#data_1").innerHTML = proximos_jogos[i].data
-     // time 2
-     document.querySelector("#imagem_time_1").src = proximos_jogos[i].times.time_a.img
-     // time 2
-     document.querySelector("#imagem_time_2").src = proximos_jogos[i].times.time_b.img
+function carrosel_esquerda() {
+    // nome do game
+    document.querySelector("#nome_jogo1").innerHTML = proximos_jogos[i].jogo
+    // campeonato
+    document.querySelector("#campeonato_1").innerHTML = proximos_jogos[i].campeonato
+    // data
+    document.querySelector("#data_1").innerHTML = proximos_jogos[i].data
+    // time 2
+    document.querySelector("#imagem_time_1").src = proximos_jogos[i].times.time_a.img
+    // time 2
+    document.querySelector("#imagem_time_2").src = proximos_jogos[i].times.time_b.img
 
- // card 2
-     // nome do game
-     document.querySelector("#nome_jogo2").innerHTML = proximos_jogos[i + 1].jogo
-     // campeonato
-     document.querySelector("#campeonato_2").innerHTML = proximos_jogos[i + 1].campeonato
-     // data
-     document.querySelector("#data_2").innerHTML = proximos_jogos[i + 1].data
-     // tim+ 1
-     document.querySelector("#imagem_time_3").src = proximos_jogos[i + 1].times.time_a.img
-     // time 2
-     document.querySelector("#imagem_time_4").src = proximos_jogos[i + 1].times.time_b.img
+    // card 2
+    // nome do game
+    document.querySelector("#nome_jogo2").innerHTML = proximos_jogos[i + 1].jogo
+    // campeonato
+    document.querySelector("#campeonato_2").innerHTML = proximos_jogos[i + 1].campeonato
+    // data
+    document.querySelector("#data_2").innerHTML = proximos_jogos[i + 1].data
+    // tim+ 1
+    document.querySelector("#imagem_time_3").src = proximos_jogos[i + 1].times.time_a.img
+    // time 2
+    document.querySelector("#imagem_time_4").src = proximos_jogos[i + 1].times.time_b.img
 }
 
-function ultimosJogos_carrosel_direita(){
+function ultimosJogos_carrosel_direita() {
     // nome do game
     document.querySelector("#nome_jogo3").innerHTML = ultimos_jogos[i + 2].jogo
     // campeonato
@@ -261,7 +261,7 @@ function ultimosJogos_carrosel_direita(){
     document.querySelector("#placar_2").innerHTML = ultimos_jogos[i + 2].times.time_b.placar
 
 
-// card 2
+    // card 2
     // nome do game
     document.querySelector("#nome_jogo4").innerHTML = ultimos_jogos[i + 3].jogo
     // campeonato
@@ -278,7 +278,7 @@ function ultimosJogos_carrosel_direita(){
     document.querySelector("#placar_4").innerHTML = ultimos_jogos[i + 3].times.time_b.placar
 }
 
-function ultimosJogos_carrosel_esquerda(){
+function ultimosJogos_carrosel_esquerda() {
     // nome do game
     document.querySelector("#nome_jogo3").innerHTML = ultimos_jogos[i].jogo
     // campeonato
@@ -294,7 +294,7 @@ function ultimosJogos_carrosel_esquerda(){
     // placar 2
     document.querySelector("#placar_2").innerHTML = ultimos_jogos[i].times.time_b.placar
 
-// card 2
+    // card 2
     // nome do game
     document.querySelector("#nome_jogo4").innerHTML = ultimos_jogos[i + 1].jogo
     // campeonato
@@ -311,28 +311,37 @@ function ultimosJogos_carrosel_esquerda(){
     document.querySelector("#placar_4").innerHTML = ultimos_jogos[i + 1].times.time_b.placar
 }
 
+// Daniel Fernandes - 11/06/2024
+// Notícias
+let news;
+const containerNoticias = document.querySelector('.noticias_container');
 fetch("db.json")
-    .then(function(response){
+    .then(function (response) {
         return response.json()
     })
-    .then(function(data){
-        // card 1
-        document.querySelector("#noticia_nomejogo1").innerHTML = data.noticias[0].jogo
-        document.querySelector("#noticia_titulo1").innerHTML = data.noticias[0].titulo
-        document.querySelector("#noticia_content1").innerHTML = data.noticias[0].conteudo
-        document.querySelector("#noticia_data1").innerHTML = data.noticias[0].data
-        document.querySelector("#foto_noticia1").src = data.noticias[0].imagem  
-        // card 2
-        document.querySelector("#noticia_nomejogo2").innerHTML = data.noticias[1].jogo
-        document.querySelector("#noticia_titulo2").innerHTML = data.noticias[1].titulo
-        document.querySelector("#noticia_content2").innerHTML = data.noticias[1].conteudo
-        document.querySelector("#noticia_data2").innerHTML = data.noticias[1].data
-        document.querySelector("#foto_noticia2").src = data.noticias[1].imagem  
+    .then(function (data) {
+        // Array de notícias
+        news = data.noticias;
 
-        // card 3
-        document.querySelector("#noticia_nomejogo3").innerHTML = data.noticias[2].jogo
-        document.querySelector("#noticia_titulo3").innerHTML = data.noticias[2].titulo
-        document.querySelector("#noticia_content3").innerHTML = data.noticias[2].conteudo
-        document.querySelector("#noticia_data3").innerHTML = data.noticias[2].data
-        document.querySelector("#foto_noticia3").src = data.noticias[2].imagem  
-    })
+        // Cria card html para as notícias
+        news.forEach(n => {
+            containerNoticias.insertAdjacentHTML('beforeend',
+                `<a href="Noticias/noticias.html" onclick="setNoticia(${n.id});"><div class="card_noticia">
+                    <img src="${n.imagem}" alt="${n.titulo}">
+                    <div class="conteudo_noticia">
+                        <h6>${n.jogo}</h6>
+                        <h3>${n.titulo}</h3>
+                        <p>${n.conteudo}</p>
+                        <p>${n.data}</p>
+                    </div>
+                </div></a>`
+            );
+        });
+    });
+
+function setNoticia(noticiaId) {
+    debugger;
+    const noticia = JSON.stringify(news.find(n => n.id === noticiaId));
+    localStorage.getItem('noticia', noticia);
+    
+}
