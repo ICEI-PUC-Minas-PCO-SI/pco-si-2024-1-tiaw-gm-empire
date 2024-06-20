66f6309ae77cdde0a5a8e87a77df35e34288b0a2
Vitor Mendonça Braga <vmbbraga5@gmail.com>
2024-06-20 19:15:34 -0300

    json times de lol e valorant completo

--- codigo/app.js
+++ codigo/app.js
@@ -1,195 +1,46 @@
-var proximos_jogos = [
-    {
-        "jogo": "League Of Legends",
-        "data": "08/06/2024 14:00",
-        "campeonato": "Cblol",
-        "times": {
-            "time_a": {
-                "nome": "Fluxo",
-                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEBJwOrlVmBIGTTziG_KZFvZfRqp7UJnfEO8Z_ji8DmA&s"
-            },
-            "time_b": {
-                "nome": "Kabum",
-                "img": "https://pm1.aminoapps.com/6743/de686a056e10c16124d5dff523a914ff0789c866v2_00.jpg"
-            }
-        }
-    },
-    {
-        "jogo": "Valorant",
-        "data": "30/06/2024 18:00",
-        "campeonato": "Champions Tour 2024: Americas",
-        "times": {
-            "time_a": {
-                "nome": "Loud",
-                "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/LOUD_logo.svg/1200px-LOUD_logo.svg.png"
-            },
-            "time_b": {
-                "nome": "Mibr",
-                "img": "https://upload.wikimedia.org/wikipedia/commons/5/53/Made_In_Brazil_logo.png"
-            }
-        }
-    },
-
-    {
-        "jogo": "Cs",
-        "data": "05/06/2024 05:00",
-        "campeonato": "YaLLa Compass 2024",
-        "times": {
-            "time_a": {
-                "nome": "Furia",
-                "img": "https://upload.wikimedia.org/wikipedia/pt/f/f9/Furia_Esports_logo.png"
-            },
-            "time_b": {
-                "nome": "Eternal Fire",
-                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsHGZIEPJ7KKcdsMqj73F7UjJkzmgVz978CQ&s"
-            }
-        }
-    },
-
-    {
-        "jogo": "Rainbow six",
-        "data": "05/06/2024 21:30",
-        "campeonato": "TPL Season 8",
-        "times": {
-            "time_a": {
-                "nome": "Karn & Co",
-                "img": "https://pbs.twimg.com/profile_images/1729702311209930752/UHvAGmKb_400x400.jpg"
-            },
-            "time_b": {
-                "nome": "Arial Arise",
-                "img": "https://pbs.twimg.com/profile_images/1724264564768829440/G5lzOLtk_400x400.jpg"
-            }
-        }
-    }
-]
-
-var ultimos_jogos = [
-    {
-        "jogo": "League of Legends",
-        "data": "02/06/2024 15:00",
-        "campeonato": "Cblol",
-        "times": {
-            "time_a": {
-                "nome": "Pain Gaming",
-                "img": "https://upload.wikimedia.org/wikipedia/pt/5/5d/PainGaming.png",
-                "placar": 0
-            },
-            "time_b": {
-                "nome": "Fluxo",
-                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEBJwOrlVmBIGTTziG_KZFvZfRqp7UJnfEO8Z_ji8DmA&s",
-                "placar": 1
-            }
-        }
-    },
-    {
-        "jogo": "Valorant",
-        "data": "05/06/2024 19:30",
-        "campeonato": "Challengers Brazil 2024",
-        "times": {
-            "time_a": {
-                "nome": "Hero base",
-                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR5A4CVohUKkco24sZ-41lbbcrbZwkU_TXxbYIz5YVpwpedT2p2qLtvRymBbk_QDBHiEg&usqp=CAU",
-                "placar": 2
-            },
-            "time_b": {
-                "nome": "Galorys Company",
-                "img": "https://static.valorantzone.gg/news/2023/06/07163614/imagem_2023-06-07_163613604.png",
-                "placar": 0
-            }
-        }
-    },
-    {
-        "jogo": "Cs",
-        "data": "05/06/2024 19:30",
-        "campeonato": "YaLLa Compass 2024",
-        "times": {
-            "time_a": {
-                "nome": "eLevate",
-                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLp_ywihY5J7njothvfrqK9b6NAWVZjHE6DA&s",
-                "placar": 2
-            },
-            "time_b": {
-                "nome": "Mythic",
-                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSooTGKkLPoQyWojZv623K8BzkQaf-HaXQo7g&s",
-                "placar": 1
-            }
-        }
-    },
-    {
-        "jogo": "Rainbow six",
-        "data": "05/06/2024 19:00",
-        "campeonato": "TPL Season 8",
-        "times": {
-            "time_a": {
-                "nome": "TSG Black",
-                "img": "https://pbs.twimg.com/profile_images/1650135927069503488/k-fdGFWU_400x400.jpg",
-                "placar": 2
-            },
-            "time_b": {
-                "nome": "Mkers",
-                "img": "https://pbs.twimg.com/profile_images/1131572156293373953/devWtScQ_400x400.png",
-                "placar": 0
-            }
-        }
-    },
-]
 
 function atualizar(){
     // card 1 - proximos jogos
-        // nome do game
-        document.querySelector("#nome_jogo1").innerHTML = proximos_jogos[0].jogo
         // campeonato
-        document.querySelector("#campeonato_1").innerHTML = proximos_jogos[0].campeonato
+        document.querySelector("#local_1").innerHTML = teamsData.times_jogos.lol.campeonatos.cblol.times[0].proximo_jogo.local
         // data
-        document.querySelector("#data_1").innerHTML = proximos_jogos[0].data
+        document.querySelector("#data_1").innerHTML = teamsData.times_jogos.lol.campeonatos.cblol.times[0].proximo_jogo.data
         // time 1
-        document.querySelector("#imagem_time_1").src = proximos_jogos[0].times.time_a.img
-        // time 2
-        document.querySelector("#imagem_time_2").src = proximos_jogos[0].times.time_b.img
-
+        for(let i = 0; i < 2; i++){
+            document.querySelector(`#imagem_time_${i + 1}`).src = teamsData.times_jogos.lol.campeonatos.cblol.times[0].proximo_jogo.times[i].logo
+        }
     // card 2
-        // nome do game
-        document.querySelector("#nome_jogo2").innerHTML = proximos_jogos[1].jogo
-        // campeonato
-        document.querySelector("#campeonato_2").innerHTML = proximos_jogos[1].campeonato
-        // data
-        document.querySelector("#data_2").innerHTML = proximos_jogos[1].data
-        // time 1
-        document.querySelector("#imagem_time_3").src = proximos_jogos[1].times.time_a.img
-        // time 2
-        document.querySelector("#imagem_time_4").src = proximos_jogos[1].times.time_b.img
+        document.querySelector("#local_2").innerHTML = teamsData.times_jogos.valorant.campeonatos.valorant_champions.times[0].proximo_jogo.local
+        document.querySelector("#data_2").innerHTML = teamsData.times_jogos.valorant.campeonatos.valorant_champions.times[0].proximo_jogo.data
+        document.querySelector(`#imagem_time_3`).src = teamsData.times_jogos.valorant.campeonatos.valorant_champions.times[0].proximo_jogo.times[0].logo
+        document.querySelector(`#imagem_time_4`).src = teamsData.times_jogos.valorant.campeonatos.valorant_champions.times[0].proximo_jogo.times[1].logo
 
     // card 3 - ultimos jogos
-        // nome do game
-        document.querySelector("#nome_jogo3").innerHTML = ultimos_jogos[0].jogo
         // campeonato
-        document.querySelector("#campeonato_3").innerHTML = ultimos_jogos[0].campeonato
+        document.querySelector("#local_3").innerHTML = teamsData.times_jogos.lol.campeonatos.Lec.times[0].proximo_jogo.local
         // data
-        document.querySelector("#data_3").innerHTML = ultimos_jogos[0].data
+        document.querySelector("#data_3").innerHTML = teamsData.times_jogos.lol.campeonatos.Lec.times[0].proximo_jogo.data
         // time 1
-        document.querySelector("#imagem_time_5").src = ultimos_jogos[0].times.time_a.img
+        document.querySelector("#imagem_time_5").src = teamsData.times_jogos.lol.campeonatos.Lec.times[0].ultimo_jogo.times[0].logo
         // time 2
-        document.querySelector("#imagem_time_6").src = ultimos_jogos[0].times.time_b.img
+        document.querySelector("#imagem_time_6").src = teamsData.times_jogos.lol.campeonatos.Lec.times[0].ultimo_jogo.times[1].logo
         // placar 1
-        document.querySelector("#placar_1").innerHTML = ultimos_jogos[0].times.time_a.placar
+        document.querySelector("#placar_1").innerHTML = teamsData.times_jogos.lol.campeonatos.Lec.times[0].ultimo_jogo.times[0].placar
         // placar 2
-        document.querySelector("#placar_2").innerHTML = ultimos_jogos[0].times.time_b.placar
+        document.querySelector("#placar_2").innerHTML = teamsData.times_jogos.lol.campeonatos.Lec.times[0].ultimo_jogo.times[1].placar
 
     // card 4
-        // nome do game
-        document.querySelector("#nome_jogo4").innerHTML = ultimos_jogos[1].jogo
-        // campeonato
-        document.querySelector("#campeonato_4").innerHTML = ultimos_jogos[1].campeonato
+        document.querySelector("#local_4").innerHTML = teamsData.times_jogos.valorant.campeonatos.valorant_challengers.times[0].ultimo_jogo.local
         // data
-        document.querySelector("#data_4").innerHTML = ultimos_jogos[1].data
+        document.querySelector("#data_4").innerHTML = teamsData.times_jogos.valorant.campeonatos.valorant_challengers.times[0].ultimo_jogo.data
         // time 1
-        document.querySelector("#imagem_time_7").src = ultimos_jogos[1].times.time_a.img
+        document.querySelector("#imagem_time_7").src = teamsData.times_jogos.valorant.campeonatos.valorant_challengers.times[0].ultimo_jogo.times[0].logo
         // time 2
-        document.querySelector("#imagem_time_8").src = ultimos_jogos[1].times.time_b.img
+        document.querySelector("#imagem_time_8").src = teamsData.times_jogos.valorant.campeonatos.valorant_challengers.times[0].ultimo_jogo.times[1].logo
         // placar 1
-        document.querySelector("#placar_3").innerHTML = ultimos_jogos[1].times.time_a.placar
+        document.querySelector("#placar_3").innerHTML = teamsData.times_jogos.valorant.campeonatos.valorant_challengers.times[0].ultimo_jogo.times[0].placar
         // placar 2
-        document.querySelector("#placar_4").innerHTML = ultimos_jogos[1].times.time_b.placar
+        document.querySelector("#placar_4").innerHTML = teamsData.times_jogos.valorant.campeonatos.valorant_challengers.times[0].ultimo_jogo.times[1].placar
 
      
 }
@@ -342,7 +193,7 @@ fetch("db.json")
 function setNoticia(noticiaId) {
     debugger;
     console.log(news)
-    const noticia = JSON.stringify(news.find(n => n.id === noticiaId));
+    const noticia = JSON.stringify(news.find(n => n.id == noticiaId));
     localStorage.setItem('noticia', noticia);
 }
 
@@ -353,6 +204,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const imagens_valorant_champions = document.querySelectorAll(".img_valorant_champions")
     const championship_challengers = teamsData.times_jogos.valorant.campeonatos.valorant_challengers;
     const imagens_valorant_challengers = document.querySelectorAll(".img_valorant_challengers")
+    const championship_Lec = teamsData.times_jogos.lol.campeonatos.Lec
+    const imagens_lol_Lec = document.querySelectorAll(".img_lol_Lec")
 
     images_lol_cblol.forEach(img => {
         const teamName = img.getAttribute('data-nome');
@@ -392,4 +245,18 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    imagens_lol_Lec.forEach(img =>{
+        const teamName = img.getAttribute('data-nome');
+        const team = championship_Lec.times.find(team => team.nome === teamName);
+        if (team) {
+            img.src = team.logo;
+            img.alt = team.nome;
+            img.addEventListener('click', () => {
+                localStorage.setItem('selectedTeam', JSON.stringify(team));
+                window.location.href = 'times/time.html';
+            });
+        }
+    })
+    
 });
